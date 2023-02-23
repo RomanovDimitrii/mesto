3ce4248e3cf515efc07b9da1cc55f290ae8feab3
Dmitrii Romanov <RomDim197@gmail.com>
2023-02-24 01:46:54 +0300

    Готово к ревью

--- skripts/script.js
+++ skripts/script.js
@@ -1,34 +1,37 @@
 let page = document.querySelector('.page');
 let editButton = page.querySelector('.profile__edit-button');
-let closeButton = page.querySelector('.edit-form__close-button');
-let saveButton = page.querySelector('.edit-form__save-button');
+let closeButton = page.querySelector('.popup__close-button');
+let saveButton = page.querySelector('.popup__save-button');
+let likeButtonImage = page.querySelector('.like-button__image');
+let saveForm = page.querySelector('.popup__container');
 
 let profileTitle = page.querySelector('.profile__title');
 let profileSubTitle = page.querySelector('.profile__subtitle');
 
 editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('click', saveProfile);
+saveForm.addEventListener('submit', saveProfile);
+likeButtonImage.addEventListener('click', fillLikeButton);
+
 function showPopup() {
-  let editForm = page.querySelector('.edit-form');
-  editForm.classList.remove('edit-form_display_hidden');
-  console.log('показал');
+  let editForm = page.querySelector('.popup');
+  editForm.classList.add('popup_opened');
+}
+
+function fillLikeButton() {
+  likeButtonImage.setAttribute('src', '../images/like_covered.svg');
 }
 
 function closePopup() {
-  let editForm = page.querySelector('.edit-form');
-  editForm.classList.add('edit-form_display_hidden');
+  let editForm = page.querySelector('.popup');
+  editForm.classList.remove('popup_opened');
 }
 
-function editProfile() {
-  let heading = document.querySelector('.edit-form__text_type_heading');
-  let description = document.querySelector('.edit-form__text_type_description');
-  profileTitle.textContent = heading.value;
-  profileSubTitle.textContent = description.value;
-}
-
-function saveProfile() {
-  console.log('Прошло');
-  editProfile();
+function saveProfile(evt) {
+  evt.preventDefault();
+  let nameInput = document.querySelector('.popup__text_type_name');
+  let jobInput = document.querySelector('.popup__text_type_job');
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
   closePopup();
 }
