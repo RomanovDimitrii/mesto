4d669c189d4dd8dfad4bb8cf181864a6d3b9f6e6
Dmitrii Romanov <RomDim197@gmail.com>
2023-03-02 02:19:34 +0300

    удален vendor

---
+++ sсripts/index.js
@@ -0,0 +1,40 @@
+let page = document.querySelector('.page');
+let profileEditButton = page.querySelector('.profile__edit-button');
+let popupCloseButton = page.querySelector('.popup__close-button');
+// let likeButtonImage = page.querySelector('.like-button__image');
+let popupForm = page.querySelector('.popup__form');
+let editForm = page.querySelector('.popup');
+let profileTitle = page.querySelector('.profile__title');
+let profileSubTitle = page.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jobInput = document.querySelector('.popup__text_type_job');
+
+// likeButtonImage.addEventListener('click', fillLikeButton);
+
+function showPopup() {
+  //функция добавляет класс попапу (display none меняет на flex), передает данные в input из текущих значений profile__title и subtitle.
+  editForm.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubTitle.textContent;
+}
+
+// function fillLikeButton() {
+//   likeButtonImage.setAttribute('src', './images/like_covered.svg');
+// }
+
+function closePopup() {
+  //функция убирает класс попапу (display flex меняет на none)
+  editForm.classList.remove('popup_opened');
+}
+
+function saveProfile(evt) {
+  //при сабмите меняет textcontent в profile__title и subtitle
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
+  closePopup();
+}
+
+profileEditButton.addEventListener('click', showPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', saveProfile);
